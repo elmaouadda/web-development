32e33775ab267c73b476e967d4b69b9f8b648164
elmaouadda <43704088+elmaouadda@users.noreply.github.com>
2023-10-09 02:34:07 +0100

    Add files via upload

---
+++ 11-todo-list.js
@@ -0,0 +1,69 @@
+//#########################################
+const todoList = [{
+  name:'Learning German',
+  dueDate: '2021-09-07'
+ },
+ {
+  name: 'Hang out with friends',
+ dueDate: '2021-10-04'
+}];
+
+renderTodoList();
+
+//#####################################
+function renderTodoList (){
+
+  
+
+  let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+
+          const todoObject = todoList[i];
+         // const name = todoObject.name;
+        //  const dueDate = todoObject.dueDate;
+          const { name, dueDate} = todoObject
+          const html = `
+              <div>${name}</div>
+              <div>${dueDate}</div>
+              <button onclick=" 
+                 todoList.splice(${i}, 1);
+                renderTodoList();
+              " class="delete-todo-button"> Delete </button> 
+              `; 
+
+            
+        todoListHTML += html;
+}
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list').innerHTML = todoListHTML; 
+
+}
+
+// #########################################################""
+function addTodo (){
+  const inputElement = document.querySelector('.js-name-input');
+
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-date-input');
+
+  const dueDate = dateInputElement.value
+
+  todoList.push({
+  //  name: name,
+   // dueDate: dueDate
+    name,
+    dueDate,
+  })
+  console.log(todoList)
+
+  inputElement.value= '';
+
+  dateInputElement.value= '';
+
+  renderTodoList ()
+}
+  
+  
